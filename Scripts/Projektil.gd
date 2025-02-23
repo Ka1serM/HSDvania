36c2c1ae30f6e3567c771711255a0d9c85a64a44
Raf Abdulla <rafeinab@outlook.de>
2025-02-23 21:35:00 +0100

    prof dealing dmg now

--- Scripts/Projektil.gd
+++ Scripts/Projektil.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
-@export var speed: float = 50
+@export var speed: float = 80
 var direction: Vector2 = Vector2.ZERO  # Standard Richtung
+
 
 func _ready():
 	#connect("area_entered", _on_body_entered)
@@ -10,12 +11,9 @@ func _ready():
 func _process(delta):
 	global_position += direction * speed * delta
 
-
-
-
 func _on_body_entered(body: Node2D) -> void:
 	print("onbodyenterd")
 	if body.name == "MainCharacter":  # Spieler-Node erkennen
-			#area.queue_free()  # Optional: Spieler Schaden zufügen oder andere Logik
+			Signalhive.emit_signal("player_damaged", 60)
 			queue_free()  # Projektil zerstören nach Kollision
 		
--- Scripts/enemy_professor.gd
+++ Scripts/enemy_professor.gd
@@ -1,12 +1,11 @@
 extends CharacterBody2D
 const SPEED = 60
 
-
 var direction = 1 
 var player_in_sight: bool = false
 var player: Node2D = null
 signal got_stomped
-@export var shoot_cooldown: float = 1.5
+@export var shoot_cooldown: float = 0.7
 @export var projectile_scene: PackedScene  # Projektil-Szene
 @onready var shoot_timer = $Timer
 @onready var shoot_marker = $Marker2D
