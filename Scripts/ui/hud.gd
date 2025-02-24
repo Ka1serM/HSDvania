7bf1c20b576f6f628654cf04f0da480693c41f67
Marcel K. <106357974+Ka1serM@users.noreply.github.com>
2025-02-24 16:54:23 +0100

    fix transition when dying more than once

--- Scripts/ui/hud.gd
+++ Scripts/ui/hud.gd
@@ -7,7 +7,9 @@ extends  Control
 @onready var continue_sfx = $ContinueSfx
 
 @onready var bafoeg_text: Label = $BafoegText
-@onready var text_fade_in_timer: Timer = $TextFadeInTimer
+
+@onready var game_over: Label = $GameOver
+@onready var transition: ColorRect = $Transition
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,29 +28,31 @@ func update_health(health):
 	health_bar.update(health)
 	
 func game_over_screen() -> void:
+	game_over.self_modulate = Color(1, 1, 1, 0)
+	game_over.visible = true
+	
 	health_bar.visible = false
 	bafoeg_text.visible = false
-	$GameOver.visible = true
 	gameover_sfx.play()
-	create_tween().tween_property($Transition, "material:shader_parameter/progress", 1.0, 1.0);
-	text_fade_in_timer.start(1.0)
+	var tween = create_tween()
+	tween.tween_property(transition, "material:shader_parameter/progress", 1.0, 1.0)
+	await tween.finished
+	create_tween().tween_property(game_over, "self_modulate", Color(1, 1, 1, 1), 4.0)
 
 func normal_HUD() -> void:
+	game_over.visible = false
+	game_over.self_modulate = Color(1, 1, 1, 1)
+	
 	health_bar.visible = true
-	$GameOver.visible = false
 	bafoeg_text.visible = true
 	bafoeg_text.text = "Bafög: 0/8"
 	GlobalVariables.bafoeg_count = 0
 	continue_sfx.play()
-	create_tween().tween_property($Transition, "material:shader_parameter/progress", 0.0, 1.0);
-	create_tween().tween_property($GameOver, "self_modulate", Color(1, 1, 1, 0), 1.0);
+	create_tween().tween_property(transition, "material:shader_parameter/progress", 0.0, 1.0)
+	create_tween().tween_property(game_over, "self_modulate", Color(1, 1, 1, 0), 1.0)
 
 
 func update_bafoeg_text(_pos,_type) -> void:
 	if !bafoeg_text.visible:
 		bafoeg_text.visible = !bafoeg_text.visible
 	bafoeg_text.text = str("Bafög: " ,GlobalVariables.bafoeg_count ,"/8")
-
-
-func _on_text_fade_in_timer_timeout() -> void:
-	create_tween().tween_property($GameOver, "self_modulate", Color(1, 1, 1, 1), 4.0);
